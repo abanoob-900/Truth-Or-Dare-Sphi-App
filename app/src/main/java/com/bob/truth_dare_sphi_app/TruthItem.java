c75d29d1733fa00b20ad9355807c4069ca5a69d2
Abanoob Samy <abanoobsamy900@gmail.com>
2023-05-31 10:31:15 +0300

    commit final

---
+++ app/src/main/java/com/bob/truth_dare_sphi_app/TruthItem.java
@@ -0,0 +1,15 @@
+package com.bob.truth_dare_sphi_app;
+
+public class TruthItem{
+
+    private String mText;
+
+    public TruthItem(String text) {
+        mText = text;
+    }
+
+    public String getmText() {
+        return mText;
+    }
+
+}
